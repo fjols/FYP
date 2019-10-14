40526e82e2bfb32ed837371ed2189aecd69c68bf
Charlie Jenkinson <charliejenko1@outlook.com>
2019-10-14 14:12:03 +0100

    Added Player 2 and shooting

---
+++ FYP/Assets/Scenes/Scripts/Players/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float m_fSpeed; // Speed of the player
+    private Rigidbody2D rbody; // Rigidbody2D component.
+    private Vector2 m_moveVel; // Movement velocity.
+    private float m_fheadingAngle; // Heading angle, used for rotation.
+    private float m_fRightHeadingAngle;
+
+    public string m_sLeftHorizontalAxis;
+    public string m_sLeftVerticalAxis;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>(); // Get the component.
+    }
+
+    void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + m_moveVel * Time.fixedDeltaTime); // Move the player.
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw(m_sLeftHorizontalAxis), -Input.GetAxisRaw(m_sLeftVerticalAxis)); // Get the axises the player is using.
+        m_moveVel = moveInput * m_fSpeed; // Move velocity.
+        m_fheadingAngle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg; // Set the heading angle.
+        transform.rotation = Quaternion.AngleAxis(m_fheadingAngle, Vector3.forward); // Use the heading angle to rotate the player in the direction they are moving.
+    }
+}
---
+++ FYP/Assets/Scenes/Scripts/Players/Shooting/Shooting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public string m_sAButton;
+    public GameObject m_bullet;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown(m_sAButton))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(m_bullet, firePoint.position, firePoint.rotation);
+    }
+}
