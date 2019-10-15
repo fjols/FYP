e24984f2efd77d2160f692e9d1e55fefdf7bda2d
Charlie Jenkinson <charliejenko1@outlook.com>
2019-10-15 19:10:42 +0100

    Added enemy and some animations

---
+++ FYP/Assets/Game Assets/Scripts/Enemies/Enemy_One/EnemyOneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    This class is for the first enemy type.
+    It will control it and animate it.
+ */
+public class EnemyOneController : MonoBehaviour
+{
+    Animator m_anim; // The animator component.
+    private bool m_bDead; // If this is true the explosion animation in the animator will be played.
+    void Start()
+    {
+        m_anim = GetComponent<Animator>(); // Get the animator component.
+        m_bDead = false; // Set it to false
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        m_bDead = true; // Set it to true it will play the death animation.
+        m_anim.SetBool("Death", m_bDead); // Set the animator to play the animation
+        Destroy(gameObject, 2.5f); // Destroy the enemy after the animations played.
+    }
+}
---
+++ FYP/Assets/Game Assets/Scripts/Players/Shooting/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    This class will make be a template for a bullet in the game.
+    It will hold values such as its speed.
+ */
+
+public class Bullet : MonoBehaviour
+{
+    // Attributes
+    [SerializeField]
+    private float m_fSpeed; // The speed the bullet will be travelling at.
+    
+    [SerializeField]
+    private float m_fLifetime; // The lifetime of the bullet.
+    
+    void Start()
+    {
+        Invoke("DestroyProjectile", m_fLifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(transform.up * m_fSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("HIT");
+    }
+
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
