bbd7a1b2aebf0836a0ea33d70058469ac955222a
Charlie Jenkinson <charliejenko1@outlook.com>
2020-01-24 00:11:19 +0000

    ADDED RANDOM FACT GENERATOR

---
+++ FYP/Assets/Game Assets/Scripts/Fact Generator/TextGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/*
+THIS CLASS WILL GENERATE RANDOM FACTS ON THE MAIN MENU.
+*/
+public class TextGeneration : MonoBehaviour
+{
+    
+    public string[] m_facts; // Array of facts to use.
+    public Text m_factText; // Text which will display the fact.
+    public string m_button; // button to generate facts.
+    void Update()
+    {
+        if(Input.GetButtonDown(m_button)) // If the set button is pressed.
+        {
+            m_factText.text = m_facts[Random.Range(0, m_facts.Length)]; // Set a random fact.
+        }
+    }
+}
