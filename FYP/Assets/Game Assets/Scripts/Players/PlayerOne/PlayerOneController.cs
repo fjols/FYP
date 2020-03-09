79afd68969bf7b4fa9d8f7ff408b26234b1977bf
Charlie Jenkinson <charliejenko1@outlook.com>
2020-03-09 01:01:17 +0000

    update

--- FYP/Assets/Game Assets/Scripts/Players/PlayerOne/PlayerOneController.cs
+++ FYP/Assets/Game Assets/Scripts/Players/PlayerOne/PlayerOneController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 
-public class PlayerOneController : MonoBehaviour
+public class PlayerOneController : NetworkBehaviour
 {
     public float m_fSpeed = 4.0f; // Speed of the player
-    public Rigidbody2D rbody; // Rigidbody2D component.
-    public Vector2 m_moveVel; // Movement velocity.
+    public Rigidbody rbody; // Rigidbody2D component.
+    public Vector3 m_moveVel; // Movement velocity.
     public float m_fheadingAngle; // Heading angle, used for rotation.
     public string m_sLeftHorizontalAxis = "LeftJoystickHorizontal"; // Holds a string that is the left joystick horizontal axis.
     public string m_sLeftVerticalAxis = "LeftJoystickVertical"; // Holds a string that is the left joystick vertical axis.
@@ -26,7 +26,7 @@ public class PlayerOneController : MonoBehaviour
 
     public Powerup _healthPowerUp;
 
-    [SyncVar] public int playersConnected;
+    private Vector3 bestGuessPosition;
 
     void Awake()
     {
@@ -35,7 +35,7 @@ public class PlayerOneController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rbody = GetComponent<Rigidbody2D>(); // Get the component.
+        rbody = GetComponent<Rigidbody>(); // Get the component.
         m_anim = GetComponent<Animator>(); // Get the animator component.
         healthText.text = "Health: " + m_iHealth;
     }
@@ -55,6 +55,31 @@ public class PlayerOneController : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw(m_sLeftHorizontalAxis), Input.GetAxisRaw(m_sLeftVerticalAxis)); // Get the axises the player is using.
         m_moveVel = moveInput.normalized * m_fSpeed; // Move velocity.
+        if(hasAuthority == false)
+        {
+            bestGuessPosition = bestGuessPosition + (m_moveVel * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, bestGuessPosition, Time.deltaTime * 10);
+            return;
+        }
+        transform.Translate(m_moveVel * Time.deltaTime);
+    }
+
+    [Command]
+    void CmdUpdateMovement(Vector3 vel, Vector3 pos)
+    {
+        transform.position = pos;
+        m_moveVel = vel;
+    }
+
+    [ClientRpc]
+    void RpcUpdateMovement(Vector3 vel, Vector3 pos)
+    {
+        if(hasAuthority)
+        {
+            return;
+        }
+        m_moveVel = vel;
+
     }
 
     void OnCollisionEnter2D(Collision2D col)
