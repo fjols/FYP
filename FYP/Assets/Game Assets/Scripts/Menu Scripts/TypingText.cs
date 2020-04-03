38c6e146278d1c9e34c91fc4c191a6e318675cca
Charlie Jenkinson <charliejenko1@outlook.com>
2020-04-03 05:38:40 +0100

    Added a cool typing text effect.

--- FYP/Assets/Game Assets/Scripts/Menu Scripts/TypingText.cs
+++ FYP/Assets/Game Assets/Scripts/Menu Scripts/TypingText.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class TypingText : MonoBehaviour
 {
-
-    public TMP_Text m_text;
-    // Start is called before the first frame update
-    void Start()
+    public string m_message; // The message to be displayed.
+    public Text m_text; // Text component.
+    public float m_typingSpeed; // Speed that the text will be displayed.
+    
+    void Awake()
     {
-        
+        m_text = GetComponent<Text>();
+        m_message = m_text.text;
+        m_text.text = "";
+        StartCoroutine("TypeText"); // Start the coroutine.
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator TypeText()
     {
-        
+        foreach(char character in m_message)
+        {
+            m_text.text += character; // Add character to the text component.
+            yield return new WaitForSeconds(m_typingSpeed); // Wait for this amount of time before adding another one.
+        }
     }
 }
